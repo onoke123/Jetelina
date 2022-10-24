51d59df95d4cd522b354488ee7927852f5fdba1e
onoke123 <onokeiji@gmail.com>
2022-10-24 21:52:40 +0900

    configの書き方を整理

--- Jetelina/lib/jetelina/JetelinaReadConfig.jl
+++ Jetelina/lib/jetelina/JetelinaReadConfig.jl
@@ -9,7 +9,8 @@
 ===#
 
 module JetelinaReadConfig
-    export  JetelinaLogfile, debugflg, JetelinaFileUploadPath, JetelinaDBPath,JetelinaDBhost,JetelinaDBport,JetelinaDBuser,JetelinaDBpassword,JetelinaDBsslmode,JetelinaDBname
+    export  JetelinaLogfile, debugflg, JetelinaFileUploadPath,
+     JetelinaDBPath,JetelinaDBhost,JetelinaDBport,JetelinaDBuser,JetelinaDBpassword,JetelinaDBsslmode,JetelinaDBname
 
     #===
         @m: JetelinaReadConfig
@@ -32,7 +33,6 @@ module JetelinaReadConfig
     ===#
     function readConfig()
         configfile = string( joinpath( @__DIR__, "config", "JetelinaConfig.cnf" ))
-        #println( "JetelinaReadCOnfig.jl config file is: ", configfile )
 
         f = open( configfile, "r" )
         l = readlines( f )
@@ -40,90 +40,68 @@ module JetelinaReadConfig
         for i = 1:size(l)[1]
             if !startswith( l[i],"#" )
                 if startswith( l[i],"logfile" )
-                    #===
-                       logfile path attribute
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    
-                    #println( "JetelinaReadCOnfig.jl logfile: ", tt )
-                
-                    global JetelinaLogfile = tt
-
+                    # logfile path attribute
+                    global JetelinaLogfile = getSetting(l[i])
                 elseif startswith( l[i], "debug" )
-                    #===
-                        debug configuration true/false
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    if tt == "true"
-                        global debugflg = true
-                    else
-                        global debugflg = false
-                    end
-
+                    # debug configuration true/false
+                    global debugflg = getSetting(l[i])
                 elseif startswith( l[i], "fileuploadpath" )
-                    #===
-                        CSV file upload path
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaFileUploadPath = tt
-                elseif startswith( l[i], "host")
-                    #===
-                        DB host 
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBhost = tt
-                elseif startswith( l[i], "port")
-                    #===
-                        DB port
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBport = tt
-                elseif startswith( l[i], "user")
-                    #===
-                        DB host 
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBuser = tt
-                elseif startswith( l[i], "password")
-                    #===
-                        DB user password
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBpassword = tt
-                elseif startswith( l[i], "sslmode")
-                    #===
-                        DB ssl mode 
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBsslmode = tt
-                elseif startswith( l[i], "dbname")
-                    #===
-                        DB name
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBname = tt
-                elseif startswith( l[i], "dbpath" )
-                    #===
-                        database path
-                    ===#
-                    t = split( l[i], "=" )
-                    tt = strip( t[2] )
-                    global JetelinaDBPath = tt
+                    # CSV file upload path
+                    global JetelinaFileUploadPath = getSetting(l[i])
+                elseif startswith( l[i], "dbtype")
+                    # DB type
+                    global JetelinaDBtype = getSetting(l[i])
+                    @info "dbtype:", JetelinaDBtype
+                    if JetelinaDBtype == "postgresql"
+                        # for PostgreSQL
+                        setPostgres(l,i+1)
+                    elseif JetelinaDBtype == "mariadb"
+                        # for MariaDB
+                    elseif JetelinaDBtype == "oracle"
+                        # for Oracle
+                    end
                 end
             else
                 # ignore as comment
             end
-
         end
     end
 
+    #===
+        get the parameter from the string
+    ===#
+    function getSetting(s)
+        t = split( s, "=" )
+        tt = strip( t[2] )
+        return tt
+    end
+
+    #===
+        PostgreSQL
+    ===#
+    function setPostgres(l,c)
+        for i = c:size(l)[1]
+            if !startswith( l[i],"#" )
+                if startswith( l[i],"host" )
+                    # DB host
+                    global JetelinaDBhost = getSetting(l[i])
+                elseif startswith( l[i], "port")
+                    # DB port
+                    global JetelinaDBport = getSetting(l[i])
+                elseif startswith( l[i], "user")
+                    # DB host 
+                    global JetelinaDBuser = getSetting(l[i])
+                elseif startswith( l[i], "password")
+                    # DB user password
+                    global JetelinaDBpassword = getSetting(l[i])
+                elseif startswith( l[i], "sslmode")
+                    # DB ssl mode 
+                    global JetelinaDBsslmode = getSetting(l[i])
+                elseif startswith( l[i], "dbname")
+                    # DB name
+                    global JetelinaDBname = getSetting(l[i])
+                end
+            end
+        end
+    end
 end
