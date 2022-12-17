8434e2999bc2f1ee0d9ce44c38c54cbadd20b7fb
onoke123 <onokeiji@gmail.com>
2022-12-17 16:32:04 +0900

    Vueやってます

--- Jetelina/public/jetelina/js/dashboard2.js
+++ Jetelina/public/jetelina/js/dashboard2.js
@@ -1,54 +1,109 @@
 const { createApp } = Vue
 
 const app = createApp({
-  data() {
-    return {
-        enterNumber: 0,
-      message: 'Hi. ',
-      usertext: ''
-    };
-  },
-  mounted() {
-    window.onload = () => {
-      const s = 'Hello, welcome and who are you?';
-      this.$refs.userInput.focus();
-      this.typing(0, s );
-    };
-  },
-  methods: {
-    typing: function (i, m) {
-      let ii = i;
-      if (i < m.length) {
-        ii++;
-        app.message = app.message + m[i]
-      } else {
-        return;
-      }
-
-      setTimeout(this.typing, 100, ii,m);
+    data() {
+        return {
+            enterNumber: 0, /* enter keyを押した回数 */
+            jetelinamessage: '', /* ユーザに表示するチャットメッセージ */
+            yourchat: '', /* ユーザが入力したチャットメッセージ text*/
+            userText: '', /* ユーザが入力したチャットメッセージ input*/
+            /* 現状の作業ステージ
+               0:ログイン前 */
+            stage: 0
+        };
     },
-
-    onKeyDown:function(){
-        if( this.enterNumber == 0 ){
-            app.message = "";
-            this.enterNumber++;
-            let ut = this.userText.trim();
-            let chunk = "";
-            if( 0 < ut.length ){
-                if( ut.indexOf( ' ' ) != -1 ){
-                    let p = ut.split( ' ' );
-                    chunk = p[p.length-1];
-                }else{
-                    chunk = ut;
+    mounted() {
+        /* 全体が読み込まれるのを待って実行 */
+        window.onload = () => {
+            /* input tagにフォーカスを当てる */
+            this.$refs.userInput.focus();
+            /* 最初のチャットメッセージを表示する　*/
+            this.typing(0, this.chooseMsg(0, "", ""));
+        };
+    },
+    methods: {
+        /* チャットに表示するメッセージを js/scenario.jsから選択する
+            i:scenarioの配列番号
+            m:メッセージに追加する文字列
+            p:選択されたチャットメッセージにmを繋げる位置　 b->before, その他->after
+        */
+        chooseMsg: function (i, m, p) {
+            const n = Math.floor(Math.random() * scenario[0].length);
+            let s = scenario[i][n];
+            if (0 < m.legnth) {
+                if (p == "b") {
+                    s = `${m} ${s}`;
+                } else {
+                    s = `${s} ${m}`;
                 }
             }
 
-            const m = `Hi ${chunk}, how are you?`;
-            this.typing(0,m);
-        }else{
-            this.$refs["userInput"].value = "";
-            this.enterNumber = 0;
+            return s;
+        },
+
+        /* チャットメッセージをタイピング風に表示する
+            i:次に表示する文字番号
+            m:表示する文字列
+        */
+        typing: function (i, m) {
+            console.log("m: ", m);
+            const t = 100;/* typing delay time */
+            let ii = i;
+            if (m != null && i < m.length) {
+                ii++;
+                app.jetelinamessage = app.jetelinamessage + m[i]
+            } else {
+                return;
+            }
+
+            setTimeout(this.typing, t, ii, m);
+        },
+
+        /* ユーザが入力するチャットボックス(input tag)でenter keyが押されたときの処理 */
+        onKeyDown: function () {
+            let ut = this.userText;
+
+            if (ut != null && 0 < ut.length) {
+                ut = ut.trim();
+                if (0 < ut.length) {
+                    app.jetelinamessage = "";
+                    app.yourchat = ut;
+                    /*
+                                        let chunk = "";
+                                        let m = "";
+                                        if (0 < ut.length) {
+                                            if (ut.indexOf(' ') != -1) {
+                                                let p = ut.split(' ');
+                                                chunk = p[p.length - 1];
+                                            } else {
+                                                chunk = ut;
+                                            }
+                                        }
+                    */
+                    if (this.stage == 0) {
+                        console.log("chk1");
+                        m = this.chooseMsg(1, "", "");
+                        console.log("m1:", m);
+                        this.typing(0, m);
+                        /*  
+                                                if (0 < chunk.length) {
+                                                    console.log("chk2");
+                                                    m = this.chooseMsg(2, chunk, "b");
+                                                }
+                        */
+                    } else {
+                        console.log("chk3");
+                        m = this.chooseMsg(2, "", "");
+                    }
+
+                    console.log("chk4");
+                    this.typing(0, m);
+                }
+
+            } else {
+                this.$refs["userInput"].value = "";
+                this.enterNumber = 0;
+            }
         }
     }
-  }
 }).mount('#jetelina') /* 実行タイミングの問題か、mount()はここでやるべきらしい */
---
+++ Jetelina/public/jetelina/js/scenario.js
@@ -0,0 +1,10 @@
+let scenario = [];
+
+/* opening */
+scenario[0] = ['Hi','Ora','Hallo'];
+
+/* login */
+scenario[1] = ['Could you give me your name?',"Let me know your name, please"];
+
+/* greeting */
+scenario[2] = ['Nice to see you', 'How are you?'];
