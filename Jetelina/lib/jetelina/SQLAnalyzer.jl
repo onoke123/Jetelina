2e3920be8d3da9d4914607cca85e04f3466d3cd4
onoke123 <onokeiji@gmail.com>
2023-02-15 16:09:01 +0900

    Graphic js and Analyze programms

---
+++ Jetelina/lib/jetelina/SQLAnalyzer.jl
@@ -0,0 +1,97 @@
+"""
+    module: SQLAnalyzer
+
+    read the log/sql.log file, then analyze the calling column status
+
+    contain functions
+
+"""
+module SQLAnalyzer
+
+    using CSV
+    using DataFrames
+    using Genie, Genie.Renderer, Genie.Renderer.Json
+    using JetelinaReadConfig, JetelinaLog
+    using ExeSql, DBDataController
+
+    """
+        read sql.log file
+            log/sql.log ex. select ftest2.id,ftest2.name from ftest2
+    """
+
+    """
+        get uniqeness
+            ex. 
+                select ftest2.id,ftest2.name from ftest2
+                select ftest2.id,ftest2.name from ftest2
+                select ftest.id,ftest.name from ftest
+                
+                -->
+                select ftest2.id,ftest2.name from ftest2
+                select ftest.id,ftest.name from ftest
+    """
+
+    """
+        count the access number in each sql
+            ex. 
+                                              sql                              access number
+            select ftest3.id,ftest2.name from ftest3 as ftest3, ftest2 as ftest2    2
+    """
+
+    """
+        shape the data
+            ex. 
+                column    combination    access number
+            ftest3.id     ftest3+ftest2      2
+            ftest2.name   ftest3+ftest2      2
+            ftest3.id     ftest4+ftest2      5
+            ftest2.name   ftest2            10
+
+            then 
+               ftest3.idが一番呼ばれたのはftet4+ftest2なので、ftest3.idはこれを採用
+               ftest2.name        〃      ftestなので、ftest2.nameはこれを採用→table変更は必要なさそう
+    """
+
+    """
+        analyze
+            ex.
+                各tableに任意のユニークintを割り振る
+                    ftest1: 1
+                    ftest2: 2
+                    ftest3: 3
+                    ftest4: 4   ....
+
+                    ftest3.idはftest4+ftest2が代表値なので → {4(ftest4)+2(ftest2)}/2(tableが２つだから)=3 ←y座標になる
+                    ftest3.idはftest3にあるので→x座標:3(ftest3)
+                    よって、ftest3.idの座標は(3,3)
+
+                    ”access number”はk-means法の"重み"として考えているけど、上記座標取得方法なら不要になる、が一応保持しておく、念のため。
+
+
+                最終的に、カラム名とカラム座標値のMatrixをファイルに格納する(一旦ね)。
+
+    """
+
+    """
+        Table Layout Change
+            analyzeに基づいてTableレイアウト変更を仮実行する。
+    """
+
+    """
+        Experimental SQL Run
+            Table Layout Changeに対してSQLを発行して、処理速度を現状と比べる。
+            結果をファイルに格納する。
+    """
+
+    """
+        Suggestion
+            Table Layout ChangeによるSQL実行がヨサゲなら
+              1.analyze結果をグラフ化
+              2.Experimental SQL Run結果をグラフ化
+              3.count the access number in each sql
+              4.shape the data
+
+            するために、JSON形式にしてfunction panelに渡す。
+            function panelのajaxはこの関数を呼び出すので、解析結果で変更が不要の時には
+            1,2では”状態OK”を返し、3,4のみのデータを返す。
+    """
