b5af4ed4b071c4ec942b6371df776cc86a953cb7
onoke123 <onokeiji@gmail.com>
2023-12-28 10:37:30 +0900

    migration works

--- Jetelina/bootstrap.jl
+++ Jetelina/bootstrap.jl
@@ -1,5 +1,9 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
 using Jetelina
-push!(Base.modules_warned_for, Base.PkgId(Jetelina))
+#push!(Base.modules_warned_for, Base.PkgId(Jetelina))
+
+# this const descri due to g5test
+const UserApp = Jetelina
+
 Jetelina.main()
--- Jetelina/src/Jetelina.jl
+++ Jetelina/src/Jetelina.jl
@@ -1,14 +1,22 @@
 module Jetelina
 
-using Genie, Logging, LoggingExtras
+#using Genie, Logging, LoggingExtras
+
+# this using descri due to g5test
+using Genie
+# this define and export due to g5test
+const up = Genie.up
+export up
 
 function main()
-  Core.eval(Main, :(const UserApp = $(@__MODULE__)))
+# these commented out are due to g5test
+
+#  Core.eval(Main, :(const UserApp = $(@__MODULE__)))
 
   Genie.genie(; context = @__MODULE__)
 
-  Core.eval(Main, :(const Genie = UserApp.Genie))
-  Core.eval(Main, :(using Genie))
+#  Core.eval(Main, :(const Genie = UserApp.Genie))
+#  Core.eval(Main, :(using Genie))
 end
 
 end
