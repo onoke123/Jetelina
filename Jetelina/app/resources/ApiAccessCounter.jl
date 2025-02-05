97963fa45e5da30020419172b741f7d2556cd0b0
onoke123 <onokeiji@gmail.com>
2025-02-05 14:48:11 +0900

    create api performance data file

--- Jetelina/app/resources/ApiAccessCounter.jl
+++ Jetelina/app/resources/ApiAccessCounter.jl
@@ -30,20 +30,20 @@ function main()
 	this function set as for kicking createAna..() from outer function.
 """
 function main()
-	interval::Integer = parse(Int,j_config.JC["analyze_interval"])
-	if isinteger(interval)
-		interval = interval*60*60 # transfer hr -> sec
-		JLog.writetoLogfile(string("ApiAccessCounter.main() start with : ",j_config.JC["analyze_interval"]," hr interval"))
+    interval::Integer = parse(Int, j_config.JC["analyze_interval"])
+    if isinteger(interval)
+        interval = interval * 60 * 60 # transfer hr -> sec
+        JLog.writetoLogfile(string("ApiAccessCounter.main() start with : ", j_config.JC["analyze_interval"], " hr interval"))
 
-		task = @async while procflg[]
-			collectApiAccessNumbers()
-			sleep(interval)
-		end
-	else
-		err = string(JC["analyze_interval"]," is not set in perfect")
-		println(err)
-		JLog.writetoLogfile("ApiAccessCounter.main() error: $err")
-	end
+        task = @async while procflg[]
+            collectApiAccessNumbers()
+            sleep(interval)
+        end
+    else
+        err = string(JC["analyze_interval"], " is not set in perfect")
+        println(err)
+        JLog.writetoLogfile("ApiAccessCounter.main() error: $err")
+    end
 end
 """
 function collectApiAccessNumbers()
@@ -53,84 +53,85 @@ function collectApiAccessNumbers()
 # Arguments
 """
 function collectApiAccessNumbers()
-	#===
-		Tips:
-			read sql.log file
-				log/sql.log 
-                    ex. 
-                        time,apino,exectime,db
-                        2024-11-05 10:46:33,js4,0.00123,postgresql
-                                   ・
-                                   ・
-	===#
-	sqllogfile = getFileNameFromLogPath(j_config.JC["sqllogfile"])
-	if !isfile(sqllogfile)
+    #===
+    		Tips:
+    			read sql.log file
+    				log/sql.log 
+                        ex. 
+                            time,apino,exectime,db
+                            2024-11-05 10:46:33,js4,0.00123,postgresql
+                                       ・
+                                       ・
+    	===#
+    sqllogfile = getFileNameFromLogPath(j_config.JC["sqllogfile"])
+    if !isfile(sqllogfile)
         procflg[] = false
         errmsg::String = """oh my, there is no log file: $sqllogfile"""
         JLog.writetoLogfile("ApiAccessCounter.collectApiAccessNumbers() error: $errmsg")
         return
-	end
+    end
 
     #===
         Tips:
             this 'reading_max_lines' is for secure.
             because who knows how much big the file is.
     ===#
-	maxrow::Int = j_config.JC["reading_max_lines"]
-	df = CSV.read(sqllogfile, DataFrame, limit = maxrow)
-	#===
-		Tips:
-			start to collect data for creating analyzing files.
-			1. api access number
-			2. database access number
-			3. api execution speed   mean/max/min
-	===#
-	#===
-		Tips:
-			get uniqeness 'apino' for '1' & '3'
-	===#
-	u_api = unique(df[:, :apino])
-	part_df = DataFrame(apino = String[], access_numbers = Int[], database = String[])
-	part_df_speed = DataFrame(apino = String[], mean = Float64[], max = Float64[], min = Float64[], database = String[])
+    maxrow::Int = j_config.JC["reading_max_lines"]
+    df = CSV.read(sqllogfile, DataFrame, limit=maxrow)
+    #===
+    		Tips:
+    			start to collect data for creating analyzing files.
+    			1. api access number
+    			2. database access number
+    			3. api execution speed   mean/max/min
+    	===#
+    #===
+    		Tips:
+    			get uniqeness 'apino' for '1' & '3'
+    	===#
+    u_api = unique(df[:, :apino])
+    part_df = DataFrame(apino=String[], access_numbers=Int[], database=String[])
+    part_df_speed = DataFrame(apino=String[], mean=Float64[], max=Float64[], min=Float64[], database=String[])
 
-	u_api_size = length(u_api)
-	if 0 < u_api_size
-		for i ∈ 1:u_api_size
-			ac = 0
-			# collect access numbers for each unique API. make "access_numbers"
-			dd = filter(:apino => x -> x == u_api[i], df)
-			ac = nrow(dd)
-			db = dd[:,:db][1]
-			speedMax = maximum(dd[:,:exectime])
-			speedMin = minimum(dd[:,:exectime])
-			speedMean = mean(dd[:,:exectime])
-			# move it to here becase it has changed each column name to each sql name.
-			push!(part_df, [u_api[i], ac, db])
-			push!(part_df_speed,[u_api[i],speedMean,speedMax,speedMin,db])
-		end
+    u_api_size = length(u_api)
+    if 0 < u_api_size
+        for i ∈ 1:u_api_size
+            ac = 0
+            # collect access numbers for each unique API. make "access_numbers"
+            dd = filter(:apino => x -> x == u_api[i], df)
+            ac = nrow(dd)
+            db = dd[:, :db][1]
+            speedMax = maximum(dd[:, :exectime])
+            speedMin = minimum(dd[:, :exectime])
+            speedMean = mean(dd[:, :exectime])
+            # move it to here becase it has changed each column name to each sql name.
+            push!(part_df, [u_api[i], ac, db])
+            push!(part_df_speed, [u_api[i], speedMean, speedMax, speedMin, db])
+        end
 
-        createAnalyzedJsonFile(part_df, JFiles.getFileNameFromLogPath(j_config.JC["apiaccesscountfile"]),1)
-        createAnalyzedJsonFile(part_df_speed, JFiles.getFileNameFromLogPath(j_config.JC["apispeedfile"]),3)
-	end
-	#===
-		Tips:
-			get uniqeness 'db' for '2'
-	===#
-	u_db = unique(df[:, :db])
-	part_df = DataFrame(database = String[], access_numbers = Int[])
-	u_db_size = length(u_db)
-	if 0 < u_db_size
-		for i ∈ 1:u_db_size
-			ac = 0
-			# collect access numbers for each unique API. make "access_numbers"
-			dd = filter(:db => x -> x == u_db[i], df)
-			ac = nrow(dd)
-			# move it to here becase it has changed each column name to each sql name.
-			push!(part_df, [u_db[i], ac])
-		end
+        createAnalyzedJsonFile(part_df, JFiles.getFileNameFromLogPath(j_config.JC["apiaccesscountfile"]), 1)
+        #createAnalyzedJsonFile(part_df_speed, JFiles.getFileNameFromLogPath(j_config.JC["apispeedfile"]),3)
+        createApiSpeedFile(part_df_speed)
+    end
+    #===
+    		Tips:
+    			get uniqeness 'db' for '2'
+    	===#
+    u_db = unique(df[:, :db])
+    part_df = DataFrame(database=String[], access_numbers=Int[])
+    u_db_size = length(u_db)
+    if 0 < u_db_size
+        for i ∈ 1:u_db_size
+            ac = 0
+            # collect access numbers for each unique API. make "access_numbers"
+            dd = filter(:db => x -> x == u_db[i], df)
+            ac = nrow(dd)
+            # move it to here becase it has changed each column name to each sql name.
+            push!(part_df, [u_db[i], ac])
+        end
 
-        createAnalyzedJsonFile(part_df, JFiles.getFileNameFromLogPath(j_config.JC["dbaccesscountfile"]),2)
-	end
+        createAnalyzedJsonFile(part_df, JFiles.getFileNameFromLogPath(j_config.JC["dbaccesscountfile"]), 2)
+    end
 end
 """
 function createAnalyzedJsonFile(df::DataFrame)
@@ -143,19 +144,61 @@ function createAnalyzedJsonFile(df::DataFrame)
 - `type::Int`: type of analyzing, 1->api access number, 2->db access number, 3->api speed
 """
 function createAnalyzedJsonFile(df::DataFrame, jsonfile::String, type::Int)
-	this_df = copy(df)
-	date = Dates.today()
-	if type == 1
-		select!(this_df, :apino, :access_numbers, :database)
-	elseif type == 2
-		select!(this_df, :database, :access_numbers)
-	elseif type == 3
-		select!(this_df, :apino, :mean, :max, :min, :database)
-	end
+    this_df = copy(df)
+    date = Dates.today()
+    if type == 1
+        select!(this_df, :apino, :access_numbers, :database)
+    elseif type == 2
+        select!(this_df, :database, :access_numbers)
+        #	elseif type == 3
+        #		select!(this_df, :apino, :mean, :max, :min, :database)
+    end
 
-	open(jsonfile, "a+") do f
-		println(f, JSON.json(Dict("result" => true, "date" => date, "Jetelina" => copy.(eachrow(this_df)))))
-	end
+    open(jsonfile, "a+") do f
+        println(f, JSON.json(Dict("result" => true, "date" => date, "Jetelina" => copy.(eachrow(this_df)))))
+    end
+end
+"""
+function createApiSpeedFile(df::DataFrame)
+
+	create csv file for each api performance data.
+
+# Arguments
+- `df::DataFrame`: target dataframe data
+"""
+function createApiSpeedFile(df::DataFrame)
+    apfs = joinpath(@__DIR__,JFiles.getFileNameFromLogPath(j_config.JC["apiperformancedatapath"]))
+    date = Dates.today()
+
+    if !isdir(apfs)
+        mkpath(apfs)
+    end
+
+    for i ∈ 1:nrow(df)
+        fname = joinpath(apfs, string(df[!, :apino][i]))
+        try
+            # write the api performance to the file
+            thefirstflg = true
+            if !isfile(fname)
+                thefirstflg = false
+            end
+
+            open(fname, "a+") do f
+                if !thefirstflg
+                    println(f, string(date, ",", j_config.JC["file_column_mean"], ',', j_config.JC["file_column_max"]), ',', j_config.JC["file_column_min"])
+                end
+
+                println(f, string(date, ",", df[!, :mean][i], ",", df[!, :max][i], ",", df[!, :min][i]))
+            end
+        catch err
+            procflg[] = false
+            println(err)
+            JLog.writetoLogfile("ApiAccessCounter.createApiSpeedFile() error: $err")
+            return
+        finally
+        end
+    end
+
 end
 """
 function stopanalyzer()
@@ -163,7 +206,7 @@ function stopanalyzer()
 	manual stopper for analyzring repeat
 """
 function stopanalyzer()
-	procflg[] = false
+    procflg[] = false
 end
 
 end
